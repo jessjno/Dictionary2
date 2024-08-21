2b873a65325ae1cac33d9a0ec3c4fc901fed08c8
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-08-21 03:24:40 +0000

    copied files over

---
+++ app.rb
@@ -0,0 +1,34 @@
+require "sinatra"
+require "sinatra/reloader"
+require "sinatra/activerecord"
+require 'unirest'
+require 'json'
+require 'uri'
+
+get("/") do
+  erb(:homepage)
+end
+
+get("/definitions") do
+  erb(:definition_form)
+end
+
+post("/get_definition") do
+  @define_word = params.fetch("user_word")
+  word_key = ENV.fetch("DEFINITION_KEY")
+
+  # Construct the URL with proper encoding
+  url = "https://wordsapiv1.p.mashape.com/words/#{URI.encode_www_form_component(@define_word)}"
+
+  # Make the API request
+  response = Unirest.get url,
+                         headers: {
+                           "X-Mashape-key" => word_key,
+                           "Accept" => "application/json"
+                         }
+
+  # Process the response
+  @string_response = response.body.to_s
+
+  erb(:definition_results)
+end
